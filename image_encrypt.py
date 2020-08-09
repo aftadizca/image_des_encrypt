878a3bf0f7337f2c21566734ca3592594dac387c
aftadizca <aftadizca@gmail.com>
2020-08-09 22:06:38 +0700

    improve speed

--- image_encrypt.py
+++ image_encrypt.py
@@ -12,17 +12,13 @@ from des import DesKey
 
 
 def process_img(img_array, processed_px, i, k, mode):
-    for j in range(0, img_array.shape[1], 2):
-        px = np.append(img_array[i, j], img_array[i, j+1]
-                       ).tobytes()
-        des = DesKey(bytes(key, encoding="utf-8"))
-        if mode == 'e':
-            enc_x = des.encrypt(px, padding=False)
-        else:
-            enc_x = des.decrypt(px, padding=False)
-        enc_x = np.frombuffer(enc_x, dtype=np.uint8)
-        processed_px[i, j] = enc_x[0:4]
-        processed_px[i, j+1] = enc_x[4:8]
+    des = DesKey(bytes(key, encoding="utf-8"))
+    if mode == 'e':
+        enc_x = des.encrypt(img_array[i].tobytes(), padding=False)
+    else:
+        enc_x = des.decrypt(img_array[i].tobytes(), padding=False)
+    enc_x = np.frombuffer(enc_x, dtype=np.uint8).reshape((480, 4))
+    processed_px[i] = enc_x
 
 
 def image_enc(mode, path, key):
@@ -56,8 +52,8 @@ def image_enc(mode, path, key):
 
 if __name__ == "__main__":
     start = perf_counter()
-    key = "12345678"
-    image_enc('e', 'test2.jpg', key)
+    key = "000000001111111122222222"
+    image_enc('e', 'download.jpeg', key)
     end = perf_counter()
     print("elapsed : ", end - start, " second")
     # print(image_enc(sys.argv[1], sys.argv[2], sys.argv[3]))

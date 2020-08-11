24b3df54bd625349c82b7d422b8380a264be3975
aftadizca <aftadizca@gmail.com>
2020-08-11 19:20:32 +0700

    use pypy

--- image_encrypt.py
+++ image_encrypt.py
@@ -1,18 +1,21 @@
 import numpy as np
-# from des2 import des, bin2hex
-# from hex2dec import hex2dec
+import SharedArray as sa
 from PIL import Image
 import convert as cvt
 import os
 import multiprocessing
-from multiprocessing import shared_memory
+# from multiprocessing import shared_memory
 from time import perf_counter
 import sys
 from des import DesKey
 
 
-def process_img(img_array, processed_px, i, qtyperprocess, k, mode):
-    for j in range(i*qtyperprocess, i*qtyperprocess+qtyperprocess):
+def process_img(img_array, processed_px, i, qtyperprocess, k, mode, modulus=0):
+    if modulus > 0:
+        ranges = range(i*qtyperprocess, i*qtyperprocess+modulus)
+    else:
+        ranges = range(i*qtyperprocess, i*qtyperprocess+qtyperprocess)
+    for j in ranges:
         des = DesKey(bytes(key, encoding="utf-8"))
         if mode == 'e':
             enc_x = des.encrypt(img_array[j].tobytes(), padding=False)
@@ -25,20 +28,34 @@ def process_img(img_array, processed_px, i, qtyperprocess, k, mode):
 def image_enc(mode, path, key):
     img_array = cvt.convert(path)
     print("Image shape : ", img_array.shape)
-    # initialize shared memory
-    shm = shared_memory.SharedMemory(
-        create=True, size=img_array.nbytes)
-    # create shared memory
-    processed_px = np.ndarray(
-        img_array.shape, dtype=img_array.dtype, buffer=shm.buf)
+
+    # # initialize shared memory
+    # shm = shared_memory.SharedMemory(
+    #     create=True, size=img_array.nbytes)
+    # # create shared memory
+    # processed_px = np.ndarray(
+    #     img_array.shape, dtype=img_array.dtype, buffer=shm.buf)
+
+    try:
+        processed_px = sa.create("a", img_array.shape, dtype=img_array.dtype)
+    except FileExistsError:
+        sa.delete("a")
+        processed_px = sa.create("a", img_array.shape, dtype=img_array.dtype)
 
     qtyperprocess = int(img_array.shape[0]/multiprocessing.cpu_count())
+    modulus = img_array.shape[0] % multiprocessing.cpu_count()
 
     # create process
     processes = []
     for i in range(0, multiprocessing.cpu_count()):
         process = multiprocessing.Process(target=process_img, args=(
             img_array, processed_px, i, qtyperprocess, key, mode,))
+        processes.append(process)
+        process.start()
+
+    if modulus > 0:
+        process = multiprocessing.Process(target=process_img, args=(
+            img_array, processed_px, multiprocessing.cpu_count(), qtyperprocess, key, mode, modulus))
         processes.append(process)
         process.start()
 
@@ -51,17 +68,19 @@ def image_enc(mode, path, key):
     filename = os.path.splitext(filename)[0].replace("_e", "")+"_"+mode+".png"
     cvt.save_image(processed_px, os.path.join(cpath, filename))
 
-    # close shared memory
-    shm.close()
-    shm.unlink()
+    # # close shared memory
+    # shm.close()
+    # shm.unlink()
+    sa.delete("a")
+
     return os.path.join(cpath, filename)
 
 
 if __name__ == "__main__":
     start = perf_counter()
-    key = "00000000"
-    image_enc('e', 'download.jpeg', key)
+    key = "000000001111111122222222"
+    image_enc('d', 'test2_e.png', key)
     end = perf_counter()
-    print("elapsed : ", end - start, " second")
+    print(f"elapsed : {end - start:.3f} second")
     # print(image_enc(sys.argv[1], sys.argv[2], sys.argv[3]))
     # print("Elapsed -", time.process_time() - t)
